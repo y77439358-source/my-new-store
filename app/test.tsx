de2eb6c6f429544d142f0ae147d69248d8d03278
y77439358-source <y77439358@gmail.com>
2026-04-29 00:37:11 +0100

    Delete app directory

--- app/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ backgroundColor: 'red', padding: '50px', textAlign: 'center', color: 'white', fontSize: '30px' }}>
-      إذا رأيت هذه الصفحة باللون الأحمر، فالمتجر يعمل!
-    </div>
-  );
-}
